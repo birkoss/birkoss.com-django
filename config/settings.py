c7c5c4732b8307bbfbe3973c5a4ea3110d4bc1d5
birkoss <admin@birkoss.com>
2016-03-31 17:17:46 -0400

    Loading local settings

--- config/settings.py
+++ config/settings.py
@@ -87,3 +87,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Load and Overwrite Settings
+try:
+    from .local_settings import *
+except ImportError as e:
+    print("No local settings configured!")
+    quit()
