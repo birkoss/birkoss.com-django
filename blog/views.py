68ee6c1b2e21184fbe6400d0abacf7763d369250
birkoss <admin@birkoss.com>
2016-04-02 06:50:33 -0400

    urls & basic templates

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,42 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Post
+
+def home(request):
+	queryset_list = Post.objects.all()
+
+	paginator = Paginator(queryset_list, 25)
+
+	page = request.GET.get('page')
+	try:
+		queryset = paginator.page(page)
+	except PageNotAnInteger:
+		queryset = paginator.page(1)
+	except EmptyPage:
+		queryset = paginator.page(paginator.num_pages)
+
+	context = {
+		'objects': queryset,
+	}
+
+	return render(request, 'blog/posts.html', context)
+
+def category(request, slug=None):
+	queryset_list = Post.objects.all()
+
+	paginator = Paginator(queryset_list, 25)
+
+	page = request.GET.get('page')
+	try:
+		queryset = paginator.page(page)
+	except PageNotAnInteger:
+		queryset = paginator.page(1)
+	except EmptyPage:
+		queryset = paginator.page(paginator.num_pages)
+
+	context = {
+		'objects': queryset,
+	}
+
+	return render(request, 'blog/posts.html', context)
